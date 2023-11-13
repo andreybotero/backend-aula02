2679a00e8aa81995a70b048a6eba5bdd0e26a3be
Andrey Botero <andreybotero564@gmail.com>
2023-11-13 09:05:29 -0300

    new commit

--- src/server.js
+++ src/server.js
@@ -9,8 +9,46 @@ const app = express();
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  return res.status(200).send({ message: "Hello World!" });
+  const { dia } = req.query;
+
+  return res.status(200).send({ message: `Hoje é ${dia}` });
 });
+
+app.post("/", (req, res) => {
+  const { nome, idade } = req.body;
+
+  if (!nome || !idade)
+    return res.status(400).send({ message: "Dados inválidos" });
+
+  return res
+    .status(200)
+
+    .send({ message: `Olá ${nome}, você tem ${idade} ano(s)` });
+});
+
+app.put("/:id", (req, res) => {
+  const { id } = req.params;
+  const { nome, idade } = req.body;
+
+  if (!nome || !idade)
+    return res.status(400).send({ message: "Dados inválidos" });
+
+  return res
+    .status(200)
+    .send({ message: `Usuário ${id} atualizado com sucesso` });
+});
+
+app.get("/:mes", (req, res) => {
+  const { mes } = req.params;
+
+  return res.status(200).send({ message: `Estamos em ${mes}` });
+});
+
+app.delete("/:id", (req, res) => {
+    const { id } = req.params;
+    
+    return res.status(200).send({ message: `Usuário ${id} deletado com sucesso` });
+    });
 
 app.listen(port, () =>
   console.log(`⚡ Server started on http://localhost:${port}`)
